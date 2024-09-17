07e3e713ec34ba5fa352e6aa81f69b11e02e2f34
Glenn Renfro <grenfro@vmware.com>
2024-09-17 15:40:15 -0400

    Update to use Tika Reader and remove hard coded tokenization

--- src/main/java/io/spring/ragbatchone/RagbatchoneConfiguration.java
+++ src/main/java/io/spring/ragbatchone/RagbatchoneConfiguration.java
@@ -1,31 +1,20 @@
 package io.spring.ragbatchone;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiConsumer;
-
+import io.spring.aibatchtools.TikaItemReader;
 import io.spring.aibatchtools.VectorStoreWriterBuilder;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -35,14 +24,11 @@ public class RagbatchoneConfiguration {
     @Value("classpath:/docs/batchtalk.txt")
     private Resource textResource;
 
-    private Map<String, Object> metadata = new HashMap<>();
-
     @Bean
-    public FlatFileItemReader <Instruction> reader(LineMapper <Instruction> lineMapper) {
-        return new FlatFileItemReaderBuilder().name("batchTalkReader")
-                .resource(textResource)
-                .lineMapper(lineMapper)
-                .build();
+    public ItemReader reader() {
+        TikaItemReader tikaItemReader = new TikaItemReader();
+        tikaItemReader.setResource(new FileSystemResource("/Users/grenfro/Downloads/scdfguide.pdf"));
+        return tikaItemReader;
     }
 
     @Bean
@@ -63,63 +49,11 @@ public class RagbatchoneConfiguration {
     }
 
     @Bean
-    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager, ItemWriter writer, LineMapper lineMapper) {
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager, ItemWriter writer) {
         return new StepBuilder("step1", jobRepository)
-                .<Instruction, Instruction> chunk(10, transactionManager)
-                .listener(new MyListener(metadata))
-                .reader(reader(lineMapper))
-                .processor(processor(metadata))
+                .<Instruction, Instruction>chunk(10, transactionManager)
+                .reader(reader())
                 .writer(writer)
                 .build();
-    }
-
-    @Bean
-    public ItemProcessor<Instruction, Instruction> processor(Map<String, Object> metadata) {
-
-        return new ItemProcessor<Instruction, Instruction>() {
-            @Override
-            public Instruction process(Instruction instruction) throws Exception {
-                metadata.forEach(new BiConsumer<String, Object>() {
-                    @Override
-                    public void accept(String s, Object s2) {
-                        instruction.getKeyData().put(s, s2.toString());
-                    }
-                });
-                return instruction;
-            }
-        };
-    }
-
-    @Bean
-    LineMapper<Instruction> lineMapper() {
-        return new LineMapper<Instruction>() {
-            @Override
-            public Instruction mapLine(String line, int lineNumber) throws Exception {
-                Instruction instruction = new Instruction();
-                instruction.setMessage(line);
-                return instruction ;
-            }
-        };
-    }
-
-    private class MyListener implements StepExecutionListener {
-
-        Map<String, Object> metadata;
-        public MyListener(Map<String, Object> metadata) {
-            this.metadata = metadata;
-        }
-        @Override
-        public void beforeStep(StepExecution stepExecution) {
-            JobParameters parameters = stepExecution.getJobExecution().getJobParameters();
-            System.out.println("ENTERING BEFORE STEP");
-            parameters.getParameters().forEach(new BiConsumer<String, JobParameter<?>>() {
-                @Override
-                public void accept(String s, JobParameter<?> jobParameter) {
-                    metadata.put(s, jobParameter.getValue());
-                }
-            });
-
-        }
-
     }
 }
