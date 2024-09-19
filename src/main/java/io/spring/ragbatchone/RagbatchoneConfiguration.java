64ebec7f5c497127eff706313233b43c876484d5
Glenn Renfro <grenfro@vmware.com>
2024-09-19 15:57:12 -0400

    Support datasource for vector DB and datasource for Task/Batch
    Update docs to reflect the latest property settings required.
    
    Rename second_datasource properties to vector_datasource
    
    This is so that it is more meaningful for users

--- src/main/java/io/spring/ragbatchone/RagbatchoneConfiguration.java
+++ src/main/java/io/spring/ragbatchone/RagbatchoneConfiguration.java
@@ -1,5 +1,7 @@
 package io.spring.ragbatchone;
 
+import javax.sql.DataSource;
+
 import io.spring.aibatchtools.TikaItemReaderBuilder;
 import io.spring.aibatchtools.VectorStoreWriterBuilder;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -12,11 +14,18 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.JdbcProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -58,5 +67,55 @@ public class RagbatchoneConfiguration {
                 .reader(reader())
                 .writer(writer(vectorStore))
                 .build();
+    }
+
+    @Bean(name = "springDataSourceProperties")
+    @ConfigurationProperties("spring.datasource")
+    @Primary
+    public DataSourceProperties springDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "vectorDataSourceProperties")
+    @ConfigurationProperties("vector.datasource")
+    public DataSourceProperties vectorSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "springDataSource")
+    @Primary
+    public DataSource dataSource(
+            @Qualifier("springDataSourceProperties") DataSourceProperties springDataSourceProperties) {
+        return DataSourceBuilder.create()
+                .driverClassName(springDataSourceProperties.getDriverClassName())
+                .url(springDataSourceProperties.getUrl())
+                .password(springDataSourceProperties.getPassword())
+                .username(springDataSourceProperties.getUsername())
+                .build();
+    }
+
+    @Bean(name = "vectorDataSource")
+    public DataSource vectorDataSource(
+            @Qualifier("vectorDataSourceProperties") DataSourceProperties vectorDataSourceProperties) {
+        return DataSourceBuilder.create()
+                .driverClassName(vectorDataSourceProperties.getDriverClassName())
+                .url(vectorDataSourceProperties.getUrl())
+                .password(vectorDataSourceProperties.getPassword())
+                .username(vectorDataSourceProperties.getUsername())
+                .build();
+    }
+
+    @Bean
+    @Primary
+    JdbcTemplate jdbcTemplate(@Qualifier("vectorDataSource") DataSource vectorDataSource, JdbcProperties properties) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(vectorDataSource);
+        JdbcProperties.Template template = properties.getTemplate();
+        jdbcTemplate.setFetchSize(template.getFetchSize());
+        jdbcTemplate.setMaxRows(template.getMaxRows());
+        if (template.getQueryTimeout() != null) {
+            jdbcTemplate.setQueryTimeout((int)template.getQueryTimeout().getSeconds());
+        }
+
+        return jdbcTemplate;
     }
 }
